efac49cdc358ea8b1a1784a198273083ea3dabb2
Mateusz Fiolka <mateusz.fiolka@gmail.com>
2011-10-12 09:39:52 +0200

    firefox keysnail config

---
+++ keysnail/.keysnail.js
@@ -0,0 +1,27 @@
+// ========================== KeySnail Init File =========================== //
+
+// You can preserve your code in this area when generating the init file using GUI.
+// Put all your code except special key, set*key, hook, blacklist.
+// ========================================================================= //
+//{{%PRESERVE%
+// Put your codes here
+//}}%PRESERVE%
+// ========================================================================= //
+
+// ========================= Special key settings ========================== //
+
+key.quitKey              = "undefined";
+key.helpKey              = "undefined";
+key.escapeKey            = "undefined";
+key.macroStartKey        = "undefined";
+key.macroEndKey          = "undefined";
+key.suspendKey           = "undefined";
+key.universalArgumentKey = "undefined";
+key.negativeArgument1Key = "undefined";
+key.negativeArgument2Key = "undefined";
+key.negativeArgument3Key = "undefined";
+
+// ================================= Hooks ================================= //
+
+// ============================= Key bindings ============================== //
+
